589b79626fa0b9c614d4a9dcdb895d76778b0164
SwiftDomain <trex1280@icloud.com>
2025-01-16 15:38:37 -0500

    test

--- PupCulture/PupCultureHome.swift
+++ PupCulture/PupCultureHome.swift
@@ -22,7 +22,7 @@ struct PupCultureHome: View {
                 .frame(width: 100, height: 100)
                 .padding(.bottom, 50)
                 
-            
+    
             TextField("Username", text: $username)
                 .padding()
                 .background(Color(.secondarySystemBackground))
